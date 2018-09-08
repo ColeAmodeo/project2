689577482889303b3c5b1fa2403d29bb2eb2baa0
Alex Jivov <alexjivov@gwu.edu>
2018-09-08 13:08:40 -0400

    added gulpfile.js for gulp minify + Sass for enhanced styling capability. Gulp tasks will automatically minify css and make it compatible with modern browsers

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+// Sass is not readable in modern browsers, 
+// so Gulp allows CSS to be written in a tree-type function 
+// within CSS. Makes everything much more readable and efficient, as well as compile it/minify it
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var changed = require('gulp-changed');
+
+
+// SCSS/CSS - Input and output folders for CSS
+
+var SCSS_SRC = './src/Assets/scss/**/*.scss'; //input
+var SCSS_DEST = './src/Assets/css'; //output
+
+//Compile SCSS
+gulp.task('compile_scss', function() {
+
+    gulp.src(SCSS_SRC)
+    .pipe(sass().on('error', sass.logError)) // compiling into standard CSS
+    .pipe(minifyCSS()) // Minify it
+    .pipe(rename({suffix: '.min'})) // changing file name to minify
+    .pipe(changed(SCSS_DEST)) // only applies to changed files
+    .pipe(gulp.dest(SCSS_DEST)); // sending to output destyination
+    
+
+});
+
+//detect changes in SCSS
+gulp.task('watch_scss', function() { // watches input folder and when it detects a change, it runs the below function
+    gulp.watch(SCSS_SRC, ['compile_scss']); //pass through as many tasks as you want on a loop
+});
+
+// Run tasks
+gulp.task('default', ['watch_scss']); // when you type 'run gulp' it defaults to gulp
