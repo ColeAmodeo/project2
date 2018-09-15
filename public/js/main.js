0bb9fcf2f534a386293b94d836e6de10795ec8fb
Cole <camodeo1@live.ca>
2018-09-15 11:44:47 -0400

    Most recent timer logic update

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 
 moment().format();
+// ***********************************************************
 // Creating environment for timer function
+// ***********************************************************
 var start;
 var startDate;
 var end;
@@ -9,8 +11,9 @@ var minutes = 0;
 var hours = 0;
 var secondsPassed = 0;
 var timerId;
-
+// ***********************************************************
 // Timer Logic
+// ***********************************************************
 function timer() {
   secondsPassed = parseInt(secondsPassed + 1);
   console.log(secondsPassed)
@@ -29,19 +32,65 @@ function timer() {
 function intervalTrigger() {
   return window.setInterval(timer, 1000)
 };
+// ***********************************************************
+//      BUTTON TRIGGER FUNCTIONS
+// ***********************************************************
+
 //Start button
 function startSession () {
   clearInterval(timerId)
   timerId = intervalTrigger()
   start = new moment()
-};
+  $("#start-btn").css("display", "none");
+  disable();
+  enablePause();
+  enableEnd();
 //End Button
 function endSession () {
- clearInterval(timerId)
- end = new moment()
- duration = moment.duration(end.diff(start))
- console.log(duration)
+  clearInterval(timerId)
+  secondsPassed = 0;
+  end = new moment()
+  duration = moment.duration(end.diff(start))
+  console.log(duration)
+  disable()
+  enableStart();
 };
+// Pause time
+function pause() {
+  clearInterval(timerId)
+  $("#start-btn").css("display", "none");
+  disable();
 
+}
+// Resume time
+function resume() {
+  timerID = intervalTrigger()
+  disable()
+}
+// ***********************************************************
+// Click Handlers
+// ***********************************************************
 $("#start-btn").off().on('click', startSession)
 $("#end-btn").off().on('click', endSession)
+
+// ***********************************************************
+// Environment for disabling buttons to stop idiots from pressing shit they shouldnt
+// ***********************************************************
+function disable(id){
+  if(!id) {
+  var buttonId = this.id;
+} else var buttonId = id
+  document.getElementById("buttonId").disabled = true
+}
+function enableStart() {
+  $("#start-btn").disabled = false;
+}
+function enableEnd() {
+  $("#end-btn").disabled = false;
+}
+function enableResume() {
+  $("#resume-btn").disabled = false;
+}
+function enablePause() {
+  $("#pause-btn").disabled = false;
+}
