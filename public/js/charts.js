231935a2e31f9344fec938537f2968981b519db5
Cole <camodeo1@live.ca>
2018-09-18 11:33:37 -0400

    couple width changes and added chart.js, it isnt connected yet
    have not gulped

---
+++ public/js/charts.js
@@ -0,0 +1,88 @@
+function pieGraph(dataPull) {
+  // Filler data
+var dataArray = [1, 2, 4, 5];
+var labelArray = ['John', 'Tim', 'Gay'];
+//Pull individual project and all employees who worked on it, use a for loop to push their respective data into names and data
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: labelArray,
+        datasets: [{
+            label: '# of hours worked',
+            data: dataArray,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+};
+function barGraph(dataPull) {
+var dataArray = [1, 2, 4, 5];
+var labelArray = ['John', 'Tim', 'Gay'];
+//Pull individual project and all employees who worked on it, use a for loop to push their respective data into names and data
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: labelArray,
+        datasets: [{
+            label: '# of hours worked',
+            data: dataArray,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+};
+
+barGraph();
+pieGraph();
