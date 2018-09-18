9ae636be6085c7f02ced6ffc3fa1d1539abffff2
Cole <camodeo1@live.ca>
2018-09-18 13:26:37 -0400

    push

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,60 +1,13 @@
 var dataArray = [];
 var labelArray = [];
 function latestSessions(dataPull) {
-  $.get("/api/sessions", function(projectData) {
-    console.log(projectData)
-    for (i = projectData.length; 10 > labelArray.length; i--) {
-      labelArray.push(projectData[i].project_desc);
-      dataArray.push(projectData[i].expected_time);
+  $.get("/api/sessions", function(sessionData) {
+    console.log(sessionData)
+    for (i = 0; i < sessionData.length; i++) {
+      labelArray.push(sessionData[i].project_id);
+      dataArray.push(sessionData[i].time_worked);
     }
-//Pull individual project and all employees who worked on it, use a for loop to push their respective data into names and data
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: labelArray,
-        datasets: [{
-            label: '# of hours worked',
-            data: dataArray,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-};
-function activeProjects(dataPull) {
-$.get("/api/projects", function(projectData) {
-  console.log(projectData)
-  for (i = 0; i < projectData.length; i++) {
-    labelArray.push(projectData[i].project_desc);
-    dataArray.push(projectData[i].expected_time);
-  }
-  console.log(labelArray)
-});
+  });
 //Pull individual project and all employees who worked on it, use a for loop to push their respective data into names and data
 var ctx = document.getElementById("myChart").getContext('2d');
 var myChart = new Chart(ctx, {
@@ -62,7 +15,7 @@ var myChart = new Chart(ctx, {
     data: {
         labels: labelArray,
         datasets: [{
-            label: '# of hours worked',
+            label: 'Latest Sessions worked',
             data: dataArray,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
@@ -95,5 +48,54 @@ var myChart = new Chart(ctx, {
 });
 };
 
-activeProjectsGraph();
-pieGraph();
+function activeProjects(dataPull) {
+$.get("/api/projects", function(projectData) {
+  console.log(projectData)
+  for (i = 0; i < projectData.length; i++) {
+    labelArray.push(projectData[i].project_desc);
+    dataArray.push(projectData[i].expected_time);
+  }
+  console.log(labelArray)
+});
+//Pull individual project and all employees who worked on it, use a for loop to push their respective data into names and data
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: labelArray,
+        datasets: [{
+            label: 'Active Projects vs Expected Completion Time',
+            data: dataArray,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+};
+
+//activeProjectsGraph();
+latestSessions();
