9bd70570c2e3f14c02bf315a35e2dfdb30c3240f
Cole <camodeo1@live.ca>
2018-09-19 15:40:09 -0400

    big push

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,36 +1,37 @@
 $(document).ready(function(){
-  var dataArray = [];
-  var labelArray = [];
-  var myChart;
 
-  function addData(chart, label, data) {
-      chart.data.labels.push(label);
-      chart.data.datasets.forEach(dataset => {
-          dataset.data.push(data);
-      });
-      chart.update();
-  }
+var dataArray = [];
+var labelArray = [];
+// RAN OUT OF TIME
+// function latestSessions(dataPull) {
+//   dataArray = [];
+//   labelArray = [];
+//   $.get("/api/sessions", function(sessionData) {
+//     console.log(sessionData)
+//     for (i = 0; i < sessionData.length; i++) {
+//       labelArray.push(sessionData[i].id);
+//       dataArray.push(sessionData[i].time_worked);
+//     }
+//   });
 
-function activeProjects() {
+function activeProjects(dataPull) {
+$.get("/api/projects", function(projectData) {
   dataArray = [];
   labelArray = [];
-$.get("/api/projects", function(projectData) {
+  console.log(projectData)
   for (i = 0; i < projectData.length; i++) {
     labelArray.push(projectData[i].project_desc);
     dataArray.push(projectData[i].expected_time);
   }
 });
-  addData(myChart1, labelArray, dataArray)
-}
-
-var ctx = document.getElementById("myChart1").getContext('2d');
-var myChart1 = new Chart(ctx, {
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: [],
+        labels: labelArray,
         datasets: [{
-            label: 'Active Projects vs Projected Completion',
-            data: [],
+            label: 'Active Projects vs Expected Completion Time',
+            data: dataArray,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -53,72 +54,20 @@ var myChart1 = new Chart(ctx, {
     options: {
         scales: {
             yAxes: [{
-              display: true,
                 ticks: {
-                  suggestedMin: 0,
+                    beginAtZero:true
                 }
             }]
         }
     }
 });
-
-
-function latestSessions() {
-  dataArray = [];
-  labelArray = [];
-  $.get("/api/sessions", function(sessionData) {
-    console.log(sessionData)
-    for (i = 0; i < sessionData.length; i++) {
-      labelArray.push(sessionData[i].project_id);
-      dataArray.push(sessionData[i].time_worked);
-    }
-  });
-  addData(myChart2, labelArray, dataArray)
 };
 
-  var ctx = document.getElementById("myChart2").getContext('2d');
-  var myChart2 = new Chart(ctx, {
-      type: 'bar',
-      data: {
-          labels: labelArray,
-          datasets: [{
-              label: 'Most Recent Sessions',
-              data: dataArray,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
-      },
-      options: {
-          scales: {
-            yAxes: [{
-              display: true,
-                ticks: {
-                  suggestedMin: 0,
-                }
-            }]
-          }
-      }
-  });
  //////////////////////
 /// Chart Hiding/Rendering
 ///////////////////////
-$("#myChart1").hide();
-$("#myChart2").hide();
+$("#myChart").hide();
+
 
 // Chart event listeners
 $('#controls-container').hide();
@@ -126,15 +75,8 @@ $('#display-buttons').on('click', function() {
   $('#controls-container').show();
 })
 $("#active-projects").on('click', function() {
-  $("#myChart2").hide()
-  $("#myChart1").show()
+  $("#myChart").show()
   activeProjects();
 } )
-$("#recent-sessions").on('click', function() {
-  $("#myChart1").hide()
-  $("#myChart2").show()
-})
 
-activeProjects();
-latestSessions();
 });
